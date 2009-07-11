8b3e0305aeec24de59a66d44c7a5eaaf616a2b85
SatheeshKumar Mohan <sathyz@gmail.com>
2009-07-12 01:49:18 +0530

    correcting problem in iimb logo name

--- php/pages/ticker.php
+++ php/pages/ticker.php
@@ -5,7 +5,7 @@
     <li><img src="images/ticker/DELL.jpg"></li>
     <li><img src="images/ticker/hpLogo.jpeg"></li>
     <li><img src="images/ticker/ibm.png"></li>
-    <li><img src="images/ticker/iimb logo8914.jpg"></li>
+    <li><img src="images/ticker/iimb.jpg"></li>
     <li><img src="images/ticker/Infosys-logo.jpg"></li>
     <li><img src="images/ticker/JockeyLogo1.jpg"></li>
     <li><img src="images/ticker/levis_logo_ver.jpg"></li>
