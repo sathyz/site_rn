10b60c924d3ad4e39c6172b5415c062e127a5f35
SatheeshKumar Mohan <sathyz@gmail.com>
2009-07-12 01:29:52 +0530

    adding logo scroller in the footer

---
+++ php/pages/ticker.php
@@ -0,0 +1,17 @@
+<!-- logo ticker -->
+<ul id="ticker01">
+    <li><img src="images/ticker/Accenture.jpeg"></li>
+    <li><img src="images/ticker/bosch.jpg"></li>
+    <li><img src="images/ticker/DELL.jpg"></li>
+    <li><img src="images/ticker/hpLogo.jpeg"></li>
+    <li><img src="images/ticker/ibm.png"></li>
+    <li><img src="images/ticker/iimb logo8914.jpg"></li>
+    <li><img src="images/ticker/Infosys-logo.jpg"></li>
+    <li><img src="images/ticker/JockeyLogo1.jpg"></li>
+    <li><img src="images/ticker/levis_logo_ver.jpg"></li>
+    <li><img src="images/ticker/logo.gif"></li>
+    <li><img src="images/ticker/madura_garments_logo.gif"></li>
+    <li><img src="images/ticker/tanishq-logo.jpg"></li>
+    <li><img src="images/ticker/vmware-logo.jpg"></li>
+    <li><img src="images/ticker/wipro.jpg"></li>
+</ul>
