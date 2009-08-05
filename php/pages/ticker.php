a494e119d2ed1f9d2ba902c833fdfb392cbe8f4f
SatheeshKumar Mohan <sathyz@gmail.com>
2009-08-05 18:15:27 +0530

    update existing files for smoothscroller

--- php/pages/ticker.php
+++ php/pages/ticker.php
@@ -1,17 +1,23 @@
 <!-- logo ticker -->
-<ul id="ticker01">
-    <li><img src="images/ticker/Accenture.jpeg"></li>
-    <li><img src="images/ticker/bosch.jpg"></li>
-    <li><img src="images/ticker/DELL.jpg"></li>
-    <li><img src="images/ticker/hpLogo.jpeg"></li>
-    <li><img src="images/ticker/ibm.png"></li>
-    <li><img src="images/ticker/iimb.jpg"></li>
-    <li><img src="images/ticker/Infosys-logo.jpg"></li>
-    <li><img src="images/ticker/JockeyLogo1.jpg"></li>
-    <li><img src="images/ticker/levis_logo_ver.jpg"></li>
-    <li><img src="images/ticker/logo.gif"></li>
-    <li><img src="images/ticker/madura_garments_logo.gif"></li>
-    <li><img src="images/ticker/tanishq-logo.jpg"></li>
-    <li><img src="images/ticker/vmware-logo.jpg"></li>
-    <li><img src="images/ticker/wipro.jpg"></li>
-</ul>
+ <div id="smallScroller">
+    <div class="scrollingHotSpotLeft"></div>
+    <div class="scrollingHotSpotRight"></div>
+    <div class="scrollWrapper">
+        <div class="scrollableArea">
+	    <img src="images/ticker/Accenture.jpeg"/>
+	    <img src="images/ticker/bosch.jpg"/>
+	    <img src="images/ticker/DELL.jpg"/>
+	    <img src="images/ticker/hpLogo.jpeg"/>
+	    <img src="images/ticker/ibm.png"/>
+	    <img src="images/ticker/iimb.jpg"/>
+	    <img src="images/ticker/Infosys-logo.jpg"/>
+	    <img src="images/ticker/JockeyLogo1.jpg"/>
+	    <img src="images/ticker/levis_logo_ver.jpg"/>
+	    <img src="images/ticker/logo.gif"/>
+	    <img src="images/ticker/madura_garments_logo.gif"/>
+	    <img src="images/ticker/tanishq-logo.jpg"/>
+	    <img src="images/ticker/vmware-logo.jpg"/>
+	    <img src="images/ticker/wipro.jpg"/>
+	</div>
+    </div>
+</div>
